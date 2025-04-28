85bfec5b187880d0be30f6657069eeee0a1fad5c
Cristian <cristian.v.chirica@gmail.com>
2025-04-28 15:25:40 +0100

    "Added code to review the use of decisions, nesting and logical operators.".

--- WEEK2/multiple_conditions.py
+++ WEEK2/multiple_conditions.py
@@ -1,30 +1,24 @@
-# An adventure Game with multiple conditions
+# Dark Forest Adventure Program using Logical AND operator
 
-# First, we need to ask the user what type of adventure they would like
-# The print Function shows the message to the user
-print("what type of adventure should i have?")
+# Asking the user what they are hearing and storing their response
+print("What did yiu hear?")
+sound = input()
 
-# The input function gets text from the user and stores it in a variable
-adventure_type = input()
-
-# Always adding a line for better readability, Keeps the empty line after input
+# Adding a blank line for better readability
 print("")
 
-# Now we are using a if statement to make decisions based on the user's input
-# The 'or' operator means " if either conditions is true"
-# So if the adventure type is EITHER "scary2 OR "short", the first message will show
-if (adventure_type == "scary") or (adventure_type == "short"):
-    # The code will run if the adventure type is "scary" or "short"
-    print("entering the dark forest!")
+# Asking the user what they saw and store their response
+print("What did you see?")
+sight = input()
 
-# The elif statement is checking if the first 'if' is False
-# If the adventure_type is Either "safe" OR "long" the second message will show
-elif (adventure_type == "safe") or (adventure_type == "long"):
-    # This code will run if the adventure type is "safe" or "long"
-    print("taking the safe route!")
+# Add a blank line for readability
+print("")
 
-# The 'else' statement runs if none of the above conditions are true
+#Check if Both conditions are true using the AND operator
+# Both conditions they must be true for the if statement to execute
+if (sound == "grrr") and (sight == "two red eyes"):
+    # This runs if both conditions are true
+    print("There is a scary creature. I should get out of here!")
 else:
-    # This code will run if the adventure is not scary, short, safe , or long
-    print("Not sure which route to take.")
-    print("")
+    # This runs if any condition is false
+    print("I am a little scared but i will continue.")
