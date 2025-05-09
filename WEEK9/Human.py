ed063da55e03a718a7c08ad05c23cf4029076a7f
Cristian <cristian.v.chirica@gmail.com>
2025-05-10 00:10:50 +0100

    Human

---
+++ WEEK9/Human.py
@@ -0,0 +1,29 @@
+
+
+class Human():
+    #Class attribute
+    MAX_ENERGY = 100
+
+    def __init__(self):
+        self.name = "Human"
+        self.age = 0
+        self.energy = self.MAX_ENERGY
+
+
+    def display(self):
+        print(f"I am {self.name}")
+
+        def grow(self):
+            self.age += 1
+
+        def eat(self, amount):
+            total_energy = self.energy + amount
+            if total_energy <= 100
+                self.energy += amount
+
+        def move(self.)
+
+if(__name__ == "__main__"):
+    human = Human()
+    human.display()
+
