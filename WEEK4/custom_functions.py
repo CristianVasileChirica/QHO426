6da19d775c097e57955e997e263b7b967528d2ae
Cristian <cristian.v.chirica@gmail.com>
2025-05-18 23:07:35 +0100

    "Added code to demonstrate the use of built-in functions: abs, range, chr.".

---
+++ WEEK4/custom_functions.py
@@ -0,0 +1,14 @@
+# The function
+def listen():
+    # asking the user what sound they heard inside the cave
+    print("What sound did you hear?")
+
+    # taking input from the user and saving it in a variable called 'sound'
+    sound = input()
+
+    # displaying the result in a fun way by adding an exclamation mark
+    print("That was a loud", sound + "!")  # we use + "!" to make it exciting
+
+
+# Call to function
+listen()  # without this line, the function won’t actually run when the program starts
